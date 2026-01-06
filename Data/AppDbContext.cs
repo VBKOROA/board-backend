d576b74bd4b1bff51cee6360671cab9eb4c42143
abko <abko121115@gmail.com>
2026-01-06 22:52:37 +0900

    댓글 모델의 PostId 필드를 필수로 설정: AppDbContext에서 IsRequired() 추가

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -14,6 +14,7 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
          .HasOne(c => c.Post)
          .WithMany(p => p.Comments)
          .HasForeignKey(c => c.PostId)
+         .IsRequired()
          .OnDelete(DeleteBehavior.Cascade);
     }
 }
